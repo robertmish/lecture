d048c0374b83ee66204e3ad5afbc06592b92ac5b
robertmish <robertmish@gmail.com>
2023-10-02 18:54:44 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//   $("#show_more_button").click(function (e) {
+//     $("#mobile_menu").css("display", "block");
+//   });
+// });
+
+$("#show_more_button").click(function () {
+  $("#mobile_menu").toggle("slow");
+});
